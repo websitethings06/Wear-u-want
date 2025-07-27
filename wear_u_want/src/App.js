d8fd509917fa0f2c0952ac6fb84f3cf354085566
Website Things 06 <websitethings06@gmail.com>
2025-07-27 16:51:06 +0530

    website chanfes

--- wear_u_want/src/App.js
+++ wear_u_want/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -19,7 +19,7 @@ function App() {
     <AuthProvider>
       <CartProvider>
         <Router>
-          <div className="App min-h-screen flex flex-col">
+          <div className="min-h-screen bg-gray-50 flex flex-col">
             <Header />
             <main className="flex-grow">
               <Routes>
