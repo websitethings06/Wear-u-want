682d4f1265165cd06044f26eadf147302508f47c
Akshay9045 <71548957+Akshay9045@users.noreply.github.com>
2025-08-06 00:10:29 +0530

    Fix accessibility issues for Netlify deployment - replace empty href with proper links

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -144,9 +144,9 @@ const Login = () => {
               </div>
 
               <div className="text-sm">
-                <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
+                <Link to="/forgot-password" className="font-medium text-blue-600 hover:text-blue-500">
                   Forgot your password?
-                </a>
+                </Link>
               </div>
             </div>
 
